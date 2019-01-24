f8c609c21950313e4d32cefb5dc5a7b8a8a3d569
Fahad Hafiz <2314120h@student.gla.ac.uk>
2019-01-24 22:29:04 +0000

    <li> -> links, + parameterised URL mapping.

--- rango/urls.py
+++ rango/urls.py
@@ -5,5 +5,6 @@ from rango import views
 #allows t call function 'url' n point t d 'index' view f d mapping in 'urlpatterns'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^about/', views.about, name='about')
+    url(r'^about/$', views.about, name='about'),
+    url(r'^category/(?P<category_name_slug>[\w\-]+)/$', views.show_category, name='show_category'),
 ]
