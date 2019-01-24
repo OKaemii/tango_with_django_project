89e112f1012f785065d92324feb390b509f56bfa
Fahad Hafiz <2314120h@student.gla.ac.uk>
2019-01-24 21:32:33 +0000

    Slug implemented, category.html added to rango

--- rango/views.py
+++ rango/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # importing catergory model
 from rango.models import Category
+from rango.models import Page
 
 def index(request):
 	# Query the database for a list of ALL categories currently stored.
@@ -26,3 +27,25 @@ def about(request):
 	context_dict = {'boldmessage': "Yip, Sip, Pip, Dip, Hip, Pick!"}
 	return render(request, 'rango/about.html', context=context_dict)
 	#return HttpResponse("Rango says here is the about page. <br/> <a href='/rango/'>Index</a>")
+def show_category(request, category_name_slug):
+	#a dictionary to pass to rendering engine
+	context_dict = {}
+
+	try:
+		#return a one model sintance or raise an exception.
+		category = Category.objects.get(slug = category_name_slug)
+
+		#retrieve all of the associated pages, filter() will return a list of page objects or empty
+		pages = Page.objects.filter(category=category)
+
+		#add results list to template context under the name of pages
+		context_dict['pages'] = pages
+		#add category object from database to template context to verify its existence.
+		context_dict['category'] = category
+	except Category.DoesNotExist:
+		#if could not find specified cat, do nothing, template display "no category" for us.
+		context_dict['category'] = None
+		context_dict['pages'] = None
+
+	#render and return to client
+	return render(request, 'rango/category.html', context_dict)
